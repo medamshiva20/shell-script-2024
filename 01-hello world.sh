f29c77d5765f07d980184b34491ed8c12d456f62
medamshiva20 <medamshiva20@gmail.com>
2024-08-05 20:41:59 +0530

    hello world script

--- 01-hello world.sh
+++ 01-hello world.sh
@@ -1,3 +1,8 @@
 #!/bin/bash 
 
 echo "Hello World,I am practicing DevOps"
+
+echo "Ramesh: Hi Suresh,Good Morning"
+echo "Suresh: Good morning Ramesh"
+echo "Ramesh: How are you Suresh"
+echo "Suresh: I am good,how are you Ramesh"
