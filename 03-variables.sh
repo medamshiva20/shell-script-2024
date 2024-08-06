7ac45301c9515e6d7cedfd4fae3125552e787ffe
medamshiva20 <medamshiva20@gmail.com>
2024-08-06 09:40:29 +0530

    shell script

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+PERSON1=Ramesh
+PERSON2=Suresh
+
+
+echo "$PERSON1: Hi $PERSON2,Good Morning"
+echo "$PERSON2: Good morning $PERSON1"
+echo "$PERSON1: How are you $PERSON2"
+echo "$PERSON2: I am good,how are you $PERSON1"
