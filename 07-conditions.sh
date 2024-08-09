b8bab6e382f71d46ccde4c80548ed567b5d018b5
medamshiva20 <medamshiva20@gmail.com>
2024-08-09 12:14:59 +0530

    conditions and package installation

--- 07-conditions.sh
+++ 07-conditions.sh
@@ -2,6 +2,7 @@
 
 NUMBER=$1
 
+#check if the number is greater than 10 or not
 if [ $NUMBER -gt 10 ]
  then
    echo "$NUMBER is greater than 10"
---
+++ 08-install-mysql.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+ then 
+   echo "ERROR:: Please run this script with root access"
+   exit 1
+ else
+   echo "INFO: You are root user"
+fi
+
+yum install mysql -y
