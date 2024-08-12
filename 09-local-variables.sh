a30e7f480aab8ba9e8397af8861475daa44786c0
medamshiva20 <medamshiva20@gmail.com>
2024-08-12 19:42:42 +0530

    local variables

--- 09-local-variables.sh
+++ 09-local-variables.sh
@@ -4,3 +4,6 @@ getNUM(){
      NUM=100 #local variable
      echo "$NUM - inside function"
 }
+
+echo "$NUM - outside function" #Here it will not call the variable(NUM) value because it is outside funcvtion only
+getNUM 
