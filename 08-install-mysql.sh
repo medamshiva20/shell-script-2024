97f178658f388a8a5364369ab6ec10318b302291
medamshiva20 <medamshiva20@gmail.com>
2024-08-13 11:31:45 +0530

    install mysql

--- 08-install-mysql.sh
+++ 08-install-mysql.sh
@@ -10,4 +10,11 @@ if [ $USERID -ne 0 ]
    echo "INFO: You are root user"
 fi
 
-yum install mysql
+yum install mysql -y
+
+if [ $? -ne 0 ]
+ then 
+   echo "Installation of mysql is error"
+ else
+   echo "Installation of mysql is success"
+fi
