47766d2785f014c928e5cfcb88f1ad026bd186a9
medamshiva20 <medamshiva20@gmail.com>
2024-08-13 12:27:46 +0530

    install mysql and postfix through functions

--- 11-functions.sh
+++ 11-functions.sh
@@ -2,13 +2,14 @@
 
 USERID=$(id -u)
 
+#This function should validate previous command and inform user it is success or failure
 VALIDATE(){
     #$1 -->It will recieve the argument
     if [ $1 -ne 0 ]
      then 
-      echo "Installation ... FAILURE"
+      echo "$2 ... FAILURE"
      else
-      echo "Installation ... SUCCESS"
+      echo "$2 ... SUCCESS"
     fi
 }
 
@@ -19,10 +20,11 @@ if [ $USERID -ne 0 ]
   echo "INFO::You are root user"
 fi
 
+#it is our responsibility again to check installation is success or not 
 yum install mysql -y 
 
-VALIDATE $?
+VALIDATE $? "Installation of MySQL"
 
 yum install postfix -y
 
-VALIDATE $?
+VALIDATE $? "Installation of postfix"
