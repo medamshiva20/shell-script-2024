5f6fb5e2daa0866112d2f19f2dbb735606235f44
medamshiva20 <medamshiva20@gmail.com>
2024-08-13 10:36:23 +0530

    install mysql

--- 08-install-mysql.sh
+++ 08-install-mysql.sh
@@ -1,21 +1,12 @@
-#!/bin/bash 
+#!/bin/bash
 
 USERID=$(id -u)
 
-if [ $USERID -ne 0 ]
+if [ $USERID -eq 0 ]
  then 
-   echo "ERROR:: Please run this script with root access"
-   exit 1
+   echo "ERROR:: Please run this script with root user"
  else
    echo "INFO: You are root user"
 fi
 
-yum install mysql -y
-
-if [ $? -ne 0 ]
- then 
-  echo "Installation of mysql is error"
-  exit 1
- else
-  echo "Installation of mysql is success"
-fi
+yum install mysql
