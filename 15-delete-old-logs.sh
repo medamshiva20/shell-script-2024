47ecd630a5b8886751ea7fa36eda7779b64e2268
medamshiva20 <medamshiva20@gmail.com>
2024-08-16 05:15:17 +0530

    delete old logs

--- 15-delete-old-logs.sh
+++ 15-delete-old-logs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 APP_LOGS_DIR=/home/centos/app-logs
-FILES_TO_DELETE=$(find $APP_LOGS_DIR -name "*.log" -type f -mtime +3)
+FILES_TO_DELETE=$(find $APP_LOGS_DIR -name "*.log" -type f -mtime +12)
 DATE=$(date +%F)
 LOGSDIR=/home/centos/shell-script-2024
 SCRIPT_NAME=$0
