ef0908777bb6fed1e629367a62f4f19d3ea8f40a
medamshiva20 <medamshiva20@gmail.com>
2024-08-16 06:17:06 +0530

    delete old logs

--- 15-delete-old-logs.sh
+++ 15-delete-old-logs.sh
@@ -2,12 +2,12 @@
 
 APP_LOGS_DIR=/home/centos/app-logs
 FILES_TO_DELETE=$(find $APP_LOGS_DIR -name "*.log" -type f -mtime +3)
-DATE=$(date +%F)
+DATE=$(date +%F:%H:%M:%S)
 LOGSDIR=/home/centos/shellscript-logs
 SCRIPT_NAME=$0
 LOGFILE=$LOGSDIR/$SCRIPT_NAME-$DATE.log
 
-echo "$FILES_TO_DELETE"
+echo "Script is executing at: $DATE"
 
 while read line 
  do 
