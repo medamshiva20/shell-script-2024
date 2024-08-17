26acdd41ee5e284c6fc84ee354f037fa78b1864f
medamshiva20 <medamshiva20@gmail.com>
2024-08-17 12:22:19 +0530

    high disk usage

--- disk_usage.sh
+++ disk_usage.sh
@@ -4,6 +4,7 @@ LOGFILE_DIRECTORY=/tmp
 DATE=$(date +%F:%H:%M:%S)
 SCRIPT_NAME=$0
 LOG_FILE=$LOGFILE_DIRECTORY/$SCRIPT_NAME-$DATE.log
+message=""
 
 R="\e[31m"
 G="\e[32m"
@@ -16,7 +17,15 @@ DISK_USAGE_THRESHOLD=1
 #IFS= internal field seperator is space.
 while IFS= read line
  do 
-    echo "output: $line"
-    
+    # This command will give you usage in number format for comparision 
+    usage =$(echo $line | awk {'print $6'} | cut -d % -f1)
+    #This command will give us partition
+    partition=$(echo $line | awk {'print $1'})
+     if [ $DISK_USAGE -gt $DISK_USAGE_THRESHOLD ]
+      then 
+        message+="HIGH DISK USAGE ON $partiion: $usage \n "
+    fi 
  done <<< $DISK_USAGE
+
+ echo "message:$message"
 
