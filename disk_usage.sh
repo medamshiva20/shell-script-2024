228d7a8a17af258ad3c98d98234b35bb0d75150a
medamshiva20 <medamshiva20@gmail.com>
2024-08-17 12:44:09 +0530

    high disk usage

--- disk_usage.sh
+++ disk_usage.sh
@@ -14,7 +14,7 @@ N="\e[0m"
 DISK_USAGE=$(df -hT | grep -vE 'tmpfs|Filesystem')
 DISK_USAGE_THRESHOLD=1
 
-while read line 
+while IFS= read line 
  do 
    usage=$(echo $line | awk {'print $6'} |cut -d % -f1)
    partion=$(echo $line | awk {'print $1'})
@@ -24,5 +24,5 @@ while read line
        message+="HIGH DISK USAGE ON $partition: $usage \n"
    fi 
  done <<< $DISK_USAGE
- echo -e "message: $message"
+ #echo -e "message: $message"
 
