171c1f4f4d454f50700b32b0d6b75a3ba7d7a004
medamshiva20 <medamshiva20@gmail.com>
2024-08-17 09:51:56 +0530

    disk usage through shell script

---
+++ disk_usage.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+
+LOGFILE_DIRECTORY=/tmp
+DATE=$(date +%F:%H:%M:%S)
+SCRIPT_NAME=$0
+LOG_FILE=$LOGFILE_DIRECTORY/$SCRIPT_NAME-$DATE.log
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+DISK_USAGE=$(df -hT | grep -vE 'tmpfs|Filesystem')
+
+echo "Disk usage is:$DISK_USAGE"
