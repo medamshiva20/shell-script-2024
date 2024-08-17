e17550328138b64e0cf577ce8ea4517cb53a09d2
medamshiva20 <medamshiva20@gmail.com>
2024-08-17 12:28:10 +0530

    high disk usage

--- disk_usage.sh
+++ disk_usage.sh
@@ -14,18 +14,9 @@ N="\e[0m"
 DISK_USAGE=$(df -hT | grep -vE 'tmpfs|Filesystem')
 DISK_USAGE_THRESHOLD=1
 
-#IFS= internal field seperator is space.
-while IFS= read line
+while read line 
  do 
-    # This command will give you usage in number format for comparision 
-    usage =$(echo $line | awk '{print $6}' | cut -d % -f1)
-    #This command will give us partition
-    partition=$(echo $line | awk {'print $1'})
-     if [ $DISK_USAGE -gt $DISK_USAGE_THRESHOLD ]
-      then 
-        message+="HIGH DISK USAGE ON $partiion: $usage \n "
-    fi 
- done <<< $DISK_USAGE
+   echo "output: $DISK_USAGE"
+ done 
 
- echo "message:$message"
 
